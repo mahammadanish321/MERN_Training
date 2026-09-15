81bac13875dcd6b62ebdf67070bbae291c35b55b
mahammadanish321 <anish130905@gmail.com>
2026-09-15 20:06:50 +0530

    Add comments and structure to foreach.js for clarity and future implementation

--- javascript-lab/learning/arry/foreach.js
+++ javascript-lab/learning/arry/foreach.js
@@ -3,3 +3,14 @@ const fruits = ["apple","banana","charry"];
 fruits.forEach(function(fruit,index){
     console.log(``)
 })
+
+
+
+// arry = [2,4,5,6,2,5,4]
+// obj = {}
+// array.forEach(element => {
+//     if element in obj 
+//     {
+//         od
+//     }
+// });
